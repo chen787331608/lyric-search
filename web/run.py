3a3a524bef5dcb4061d004ea7a7993d286c3a76e
LonJE <chen787331608@163.com>
2018-05-30 15:51:52 +0800

    add gevent for concurrency

--- web/run.py
+++ web/run.py
@@ -3,12 +3,15 @@
 # Author: LonJE
 
 import click
+from gevent.pywsgi import WSGIServer
 from app import app
 
 @click.command()
 @click.option('--port', '-p', default=5000, help='listening port')
 def run(port):
-    app.run(debug=True, port=port)
+    # app.run(debug=True, port=port)
+    http_server = WSGIServer(('127.0.0.1', port), app)
+    http_server.serve_forever()
 
 if __name__ == '__main__':
     run()
